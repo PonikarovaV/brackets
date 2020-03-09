bbf3a9abb86fb8b8e83708cc211ca40c24a304c1
Vanda Ponikarova <ponikarova@gmail.com>
2020-03-09 22:02:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strConfig = bracketsConfig.map( el => { 
+        return el.join('');
+    });
+    function checkString(string) {
+        if (string === '') {
+          return true;
+        }
+        if (string !== '') {
+            let findPair = strConfig.find( el => {
+                if (string.includes(el)) {
+                return el;
+                }
+            });
+    
+            if (findPair === undefined) {
+                return false;
+            } else {
+                return checkString(string.replace(findPair, ''));
+            }
+        }
+    }
+    return checkString(str);
 }
